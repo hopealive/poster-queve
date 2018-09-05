a02d2400039ca119dbef817640e94102075eb73c
Gregzorb <websitegdp@gmail.com>
2018-09-05 17:58:59 +0300

    Move jsImgSlider from version. Gitignore settings. Force in future

---
+++ lib/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Description of Config
+ *
+ * @author gregzorb
+ */
+class Config
+{
+
+        public $configs = [
+            'url' => 'https://gregzorb.joinposter.com/api/',
+            'token' => '0690289d0f354269eda7a6537a9cec6b',
+            'doneComment' => '+',
+        ];
+
+}
